727b3319e14fa55ab237e2b928487c9f35e2f707
Bartosz <bg@bartoszgajda.com>
2019-03-22 21:02:58 +0000

    Initial Commit.

---
+++ src/main/java/com/gcu/wpd2/Wpd2Application.java
@@ -0,0 +1,13 @@
+package com.gcu.wpd2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wpd2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Wpd2Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/gcu/wpd2/Wpd2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gcu.wpd2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Wpd2ApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
