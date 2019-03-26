b321e9542848eba8e28b36309106d04ae0423ef1
Bartosz <bg@bartoszgajda.com>
2019-03-26 16:10:56 +0000

    Update repo and seeder for User

--- src/main/java/com/gcu/wpd2/db/UserRepository.java
+++ src/main/java/com/gcu/wpd2/db/UserRepository.java
@@ -4,6 +4,5 @@ import com.gcu.wpd2.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
-  public User findByEmail(String email);
-  public User findByFirstNameAndLastName(String firstName, String lastName);
+  public User findByUsername(String username);
 }
