97a1a8c17322d7e738d250f2311b1f090231d095
Bartosz Gajda <bg@bartoszgajda.com>
2019-04-01 20:41:35 +0100

    Remove all Role occurrences.

--- src/main/java/com/gcu/wpd2/Main.java
+++ src/main/java/com/gcu/wpd2/Main.java
@@ -1,38 +1,11 @@
 package com.gcu.wpd2;
 
-import com.gcu.wpd2.models.Role;
-import com.gcu.wpd2.db.RoleRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-    }
-    
-    @Bean
-    CommandLineRunner init(RoleRepository roleRepository) {
-
-        return args -> {
-
-            Role adminRole = roleRepository.findByRole("ADMIN");
-            if (adminRole == null) {
-                Role newAdminRole = new Role();
-                newAdminRole.setRole("ADMIN");
-                roleRepository.save(newAdminRole);
-            }
-            
-            Role userRole = roleRepository.findByRole("USER");
-            if (userRole == null) {
-                Role newUserRole = new Role();
-                newUserRole.setRole("USER");
-                roleRepository.save(newUserRole);
-            }
-        };
-
     }
 }
--- src/main/java/com/gcu/wpd2/db/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gcu.wpd2.db;
-
-import com.gcu.wpd2.models.Role;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RoleRepository extends MongoRepository<Role, String> {
-    
-    Role findByRole(String role);
-}
