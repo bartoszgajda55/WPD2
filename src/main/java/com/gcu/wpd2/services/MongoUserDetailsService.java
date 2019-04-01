f4e626447acc751d2e7cd420ac85dd0677f13acf
Bartosz Gajda <bg@bartoszgajda.com>
2019-04-01 20:33:09 +0100

    Remove need for checking user roles. Use default one.

--- src/main/java/com/gcu/wpd2/services/MongoUserDetailsService.java
+++ src/main/java/com/gcu/wpd2/services/MongoUserDetailsService.java
@@ -21,48 +21,37 @@ import org.springframework.stereotype.Service;
 @Service
 public class MongoUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private RoleRepository roleRepository;
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+  @Autowired
+  private UserRepository userRepository;
+  @Autowired
+  private RoleRepository roleRepository;
+  @Autowired
+  private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public User findUserByEmail(String email) {
-        return userRepository.findByEmail(email);
+  public User findUserByEmail(String email) {
+    return userRepository.findByEmail(email);
+  }
+
+  public void saveUser(User user) {
+    user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+    Role userRole = roleRepository.findByRole("ADMIN");
+    user.setRoles(new HashSet<>(Arrays.asList(userRole)));
+    userRepository.save(user);
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    User user = userRepository.findByEmail(email);
+    if(user != null) {
+      List<GrantedAuthority> authorities = Arrays.asList(new SimpleGrantedAuthority("user"));
+      return buildUserForAuthentication(user, authorities);
+    } else {
+      throw new UsernameNotFoundException("username not found");
     }
+  }
 
-    public void saveUser(User user) {
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        Role userRole = roleRepository.findByRole("ADMIN");
-        user.setRoles(new HashSet<>(Arrays.asList(userRole)));
-        userRepository.save(user);
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-
-        User user = userRepository.findByEmail(email);  
-        if(user != null) {
-            List<GrantedAuthority> authorities = getUserAuthority(user.getRoles());
-            return buildUserForAuthentication(user, authorities);
-        } else {
-            throw new UsernameNotFoundException("username not found");
-        }
-    }
-
-    private List<GrantedAuthority> getUserAuthority(Set<Role> userRoles) {
-        Set<GrantedAuthority> roles = new HashSet<>();
-        userRoles.forEach((role) -> {
-            roles.add(new SimpleGrantedAuthority(role.getRole()));
-        });
-
-        List<GrantedAuthority> grantedAuthorities = new ArrayList<>(roles);
-        return grantedAuthorities;
-    }
-
-    private UserDetails buildUserForAuthentication(User user, List<GrantedAuthority> authorities) {
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
-    }
+  private UserDetails buildUserForAuthentication(User user, List<GrantedAuthority> authorities) {
+    return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+  }
 
 }
