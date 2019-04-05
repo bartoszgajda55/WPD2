15c331713002ac40871e7d324d72d37522d49026
Bartosz Gajda <bg@bartoszgajda.com>
2019-04-05 21:31:13 +0100

    Add Dashboard controller. Move the mapping for the view to new controller.

--- src/main/java/com/gcu/wpd2/controllers/AuthenticationController.java
+++ src/main/java/com/gcu/wpd2/controllers/AuthenticationController.java
@@ -2,15 +2,12 @@ package com.gcu.wpd2.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+
+import com.gcu.wpd2.db.UserRepository;
 import com.gcu.wpd2.models.User;
 import com.gcu.wpd2.services.MongoUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +16,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AuthenticationController {
-
     @Autowired
-    private MongoUserDetailsService userService;
+    private UserRepository userRepository;
+    @Autowired
+    private MongoUserDetailsService userDetailsService;
     @Autowired
     private AuthenticationManager authenticationManager;
 
@@ -44,7 +42,7 @@ public class AuthenticationController {
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
     public ModelAndView createUserFromSignupForm(@Valid User user, BindingResult bindingResult, HttpServletRequest request) {
         ModelAndView modelAndView = new ModelAndView();
-        User userExists = userService.findUserByEmail(user.getEmail());
+        User userExists = userRepository.findByEmail(user.getEmail());
         if (userExists != null) {
             bindingResult.rejectValue("email", "error.user",
               "There is already a user registered with the username provided");
@@ -53,22 +51,10 @@ public class AuthenticationController {
             modelAndView.setViewName("signup");
             return modelAndView;
         }
-        userService.saveUser(user);
+        userDetailsService.saveUserAndHashPassword(user);
         modelAndView.addObject("successMessage", "User has been registered successfully");
         modelAndView.addObject("user", new User());
         modelAndView.setViewName("login");
-        return modelAndView;
-    }
-
-    @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
-    public ModelAndView viewDashboardPage() {
-        ModelAndView modelAndView = new ModelAndView();
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        User user = userService.findUserByEmail(auth.getName());
-        modelAndView.addObject("currentUser", user);
-        modelAndView.addObject("fullName", "Welcome " + user.getFirstName());
-        modelAndView.addObject("adminMessage", "Dashboard available only for logged in Users");
-        modelAndView.setViewName("dashboard");
         return modelAndView;
     }
     
---
+++ src/main/java/com/gcu/wpd2/controllers/DashboardController.java
@@ -0,0 +1,27 @@
+package com.gcu.wpd2.controllers;
+
+import com.gcu.wpd2.db.UserRepository;
+import com.gcu.wpd2.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DashboardController {
+  @Autowired
+  private UserRepository userRepository;
+
+  @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
+  public ModelAndView getDashboardPage() {
+    ModelAndView modelAndView = new ModelAndView();
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    User user = userRepository.findByEmail(auth.getName());
+    modelAndView.addObject("currentUser", user);
+    modelAndView.setViewName("dashboard");
+    return modelAndView;
+  }
+}
