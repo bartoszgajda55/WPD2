df4dfef95b71f1edfc7bffb5999290720d370e30
hawrot <mhawro200@caledonian.ac.uk>
2019-04-24 00:35:33 +0100

    Add createMilestone method

--- src/main/java/com/gcu/wpd2/controllers/MilestoneController.java
+++ src/main/java/com/gcu/wpd2/controllers/MilestoneController.java
@@ -1,14 +1,23 @@
 package com.gcu.wpd2.controllers;
 
+import com.gcu.wpd2.models.Milestone;
+import com.gcu.wpd2.models.Project;
+import com.gcu.wpd2.models.User;
 import com.gcu.wpd2.services.MilestoneService;
 import com.gcu.wpd2.services.ProjectService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 public class MilestoneController {
@@ -26,6 +35,15 @@ public class MilestoneController {
         modelAndView.addObject("milestone",milestoneService.getByID(milestoneId));
         modelAndView.setViewName("project/view");
         return  modelAndView;
+    }
+
+    @RequestMapping(value = "/milestone/add", method = RequestMethod.POST)
+    public ModelAndView createMilestone(@Valid Milestone milestone, BindingResult bindingResult){
+        ModelAndView modelAndView = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Project project = projectService.getByName(auth.getName());
+        milestoneService.saveToProject(milestone, project);
+        return modelAndView;
     }
 
 }
