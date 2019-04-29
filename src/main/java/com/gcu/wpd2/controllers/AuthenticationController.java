6235a159b2ae404674dd94100d529f8fbf6b293f
Bartosz Gajda <bg@bartoszgajda.com>
2019-04-29 19:08:23 +0100

    Attach the currentUser to Auth Controller.

--- src/main/java/com/gcu/wpd2/controllers/AuthenticationController.java
+++ src/main/java/com/gcu/wpd2/controllers/AuthenticationController.java
@@ -4,8 +4,9 @@ import javax.validation.Valid;
 import com.gcu.wpd2.models.User;
 import com.gcu.wpd2.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,6 +21,7 @@ public class AuthenticationController {
     public ModelAndView viewLoginPage() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject(new User());
+        modelAndView.addObject("currentUser", getCurrentUser());
         modelAndView.setViewName("login");
         return modelAndView;
     }
@@ -29,6 +31,7 @@ public class AuthenticationController {
         ModelAndView modelAndView = new ModelAndView();
         User user = new User();
         modelAndView.addObject("user", user);
+        modelAndView.addObject("currentUser", getCurrentUser());
         modelAndView.setViewName("signup");
         return modelAndView;
     }
@@ -47,6 +50,7 @@ public class AuthenticationController {
         }
         userService.saveUserAndHashPassword(user);
         modelAndView.addObject("registered", true);
+        modelAndView.addObject("currentUser", getCurrentUser());
         modelAndView.setViewName("redirect:/login");
         return modelAndView;
     }
@@ -55,13 +59,20 @@ public class AuthenticationController {
     public ModelAndView viewHomePage() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("home");
+        modelAndView.addObject("currentUser", getCurrentUser());
         return modelAndView;
     }
 
     @RequestMapping(value = "/404", method = RequestMethod.GET)
     public ModelAndView view404Page() {
         ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("currentUser", getCurrentUser());
         modelAndView.setViewName("404");
         return modelAndView;
+    }
+
+    User getCurrentUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return userService.findByEmail(auth.getName());
     }
 }
